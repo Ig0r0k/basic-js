33fbff53b24afb438fdac85ab5853e9e8891b775
Ihor Holysh <58191199+Ig0r0k@users.noreply.github.com>
2020-10-31 00:57:31 +0200

    feat: add solution transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('THROWN');
+  let itemCount = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      if (++i in arr) itemCount[i] = 0;
+    } else if (arr[i] === '--double-next') {
+      if (++i in arr) itemCount[i] = 2;
+    } else if (arr[i] === '--discard-prev') {
+      if ((i - 1) in itemCount && itemCount[i - 1]) {
+        itemCount[i - 1]--;
+      }
+    } else if (arr[i] === '--double-prev') {
+      if ((i - 1) in itemCount && itemCount[i - 1]) {
+        itemCount[i - 1]++;
+      }
+    } else {
+      itemCount[i] = 1;
+    }
+  }
+
+  let result = [];
+  for (let i = 0; i < itemCount.length; i++) {
+    while (itemCount[i]) {
+      result.push(arr[i]);
+      itemCount[i]--;
+    }
+  }
+  return result;
 };
